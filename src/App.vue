9764256fb5068a88f728a4b8802a88167532ca25
qingzhou71 <42526511+qingzhou71@users.noreply.github.com>
2019-03-15 16:11:22 +0800

    简单的demo (#1)

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
   </div>
 </template>
 
+
 <script>
 export default {
   name: 'App'
@@ -18,6 +19,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/containers/login.vue
+++ src/containers/login.vue
@@ -1,15 +1,70 @@
 <template>
   <div class="hello">
-   
    <a-button type='primary'>test</a-button>
    <router-link to='/register'> to test</router-link>
+   <a-card>
+    <a-form
+    id="components-form-demo-normal-login"
+    :form="form"
+    class="login-form"
+    @submit="handleSubmit"
+  >
+    <a-form-item class="login-form-item">
+      <a-input
+        v-decorator="[
+          'userName',
+          { rules: [{ required: true, message: '你还没有输入账号!' }] }
+        ]"
+        placeholder="请输入账号"
+      >
+        <a-icon
+          slot="prefix"
+          type="user"
+          style="color: rgba(0,0,0,.25)"
+        />
+      </a-input>
+    </a-form-item>
+    <a-form-item class="login-form-item">
+      <a-input
+        v-decorator="[
+          'password',
+          { rules: [{ required: true, message: '你还没有输入密码!' }] }
+        ]"
+        type="password"
+        placeholder="请输入密码"
+      >
+        <a-icon
+          slot="prefix"
+          type="lock"
+          style="color: rgba(0,0,0,.25)"
+        />
+      </a-input>
+    </a-form-item>
+    <a-form-item>
+      <a
+        class="login-form-forgot"
+        href=""
+      >
+        忘记密码
+      </a>
+      <a-button
+        type="primary"
+        html-type="submit"
+        class="login-form-button"
+      >
+        登录
+      </a-button>
+    </a-form-item>
+  </a-form>
+   </a-card>
   </div>
 </template>
 
 <script>
-
-
 export default {
+  beforeCreate () {
+    this.form = this.$form.createForm(this);
+  },
   name: 'login',
   data () {
     return {
@@ -17,10 +72,43 @@ export default {
       test:'i will'
     }
   },
-  method:{
-    
+  methods:{
+     handleSubmit (e) {
+      e.preventDefault();
+      this.form.validateFields((err, values) => {
+        if (!err) {
+          console.log('Received values of form: ', values);
+          this.$router.push({path: '/register'});
+        }
+      });
+    },
   }
 }
 </script>
+
+<style>
+.hello{
+width: 100%;
+}
+#components-form-demo-normal-login .login-form {
+  max-width: 300px;
+}
+#components-form-demo-normal-login .login-form-item {
+  width: 100%;
+  margin: 10px 0;
+}
+#components-form-demo-normal-login .login-form-forgot {
+  display: block;
+  float: right;
+}
+#components-form-demo-normal-login .login-form-button {
+  width: 100%;
+}
+.ant-card-bordered{
+  border: none;
+  width: 80%;
+  margin: 0 auto;
+}
+</style>
 
 
