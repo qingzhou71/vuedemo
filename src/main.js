82658b60802e619de74eb016bf80f814bf2fe077
qingzhou <zq19721121@163.com>
2019-05-19 13:06:53 +0800

    ！！！

--- src/main.js
+++ src/main.js
@@ -4,13 +4,7 @@ import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-<<<<<<< HEAD
 import {Button,Form,Checkbox,Input,Icon,Card,Drawer,Menu,Divider,Select,Row,Col,Table,List,Popconfirm,Modal,Steps,Carousel} from 'ant-design-vue'
-=======
-
-import {Button,Form,Checkbox,Input,Icon,Card,Drawer,Menu,Divider,Select,Row,Col,Table,List,Popconfirm,Modal,Steps} from 'ant-design-vue'
-
->>>>>>> e6c75c1cdd8275d4a803b379c6d79ee8758eacfb
 import 'ant-design-vue/dist/antd.css'
 
 Vue.config.productionTip = false
